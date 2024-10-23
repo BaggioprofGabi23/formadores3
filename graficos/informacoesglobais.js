f30399df313e8a6d5e21c3a9232c27e435a89a7c
GABRIELA MARIA BAGGIO <125301132+BaggioprofGabi23@users.noreply.github.com>
2024-10-23 17:40:37 -0300

    dh

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -8,7 +8,7 @@ async function vizualizarInformacoesGlobais() {
 
 const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.transporte-dados-globalThis} </span> de pessoas e que aproximadamente 
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.transporte-dados-globalThis} </span> o transporte para o trabalho se torna parte essencial da rotina diária de 2.000.000.000 dessas pessoas. Este deslocamento, em média, consome cerca de 0,9 hora de cada trabalhador. Em uma escala global,é fácil perceber como o transporte impacta bilhões de vidas.
     <span> ${dados.transporte-dados-globais} </span> estão empregadas em serviços registrados. Eles trabalham 
     <span> ${dados.tranposte_numeros} </span> horas por semana.`
     const container = document.getElementById('graficos-container')
