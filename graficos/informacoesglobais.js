4dd8f3d3883df7904108a187ef1b4b98781e6b7a
GABRIELA MARIA BAGGIO <125301132+BaggioprofGabi23@users.noreply.github.com>
2024-10-23 17:22:49 -0300

    fghf

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -7,8 +7,8 @@ async function vizualizarInformacoesGlobais() {
 }
 
 const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
+    paragrafo.classList.add('graficos-container')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${transporte-dados-globalThis.json} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
     const container = document.getElementById('graficos-container')
 container.appendChield(paragrafo):
 
