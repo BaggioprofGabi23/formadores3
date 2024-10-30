4376c78c18fc299452ed9402bbbcf9fc5a16eeeb
GABRIELA MARIA BAGGIO <125301132+BaggioprofGabi23@users.noreply.github.com>
2024-10-30 16:27:55 -0300

    çç

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -1,17 +1,16 @@
-const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json'
 
-async function vizualizarInformacoesGlobais() {
+async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
 }
 
 const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.transporte-dados-globalThis} </span> o transporte para o trabalho se torna parte essencial da rotina diária de 2.000.000.000 dessas pessoas. Este deslocamento, em média, consome cerca de 0,9 hora de cada trabalhador. Em uma escala global,é fácil perceber como o transporte impacta bilhões de vidas.
-    <span> ${dados.transporte-dados-globais} </span> estão empregadas em serviços registrados. Eles trabalham 
-    <span> ${dados.tranposte_numeros} </span> horas por semana.`
-    const container = document.getElementById('graficos-container')
-container.appendChield(paragrafo):
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo);
 
-vizualizarInformacoesGlobais()
+
+visualizarInformacoesGlobais()
